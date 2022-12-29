162bf7ae2327efa0bb2e8c2a26ac9815b8d1a057
PlezDO <119824546+PlezDO@users.noreply.github.com>
2022-12-29 00:43:43 -0500

    reformatted template java package

---
+++ src/main/java/com/mwdev/open/ranker/MWRanker.java
@@ -0,0 +1,12 @@
+package com.mwdev.open.ranker;
+
+/**
+ * @author Plez Ownby
+ */
+public class MWRanker {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
