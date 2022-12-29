a02f0f5af9af2cfaf9ad8172d7dbfb699784baae
PlezDO <119824546+PlezDO@users.noreply.github.com>
2022-12-29 01:22:50 -0500

    set up for stat tracking

--- src/main/java/com/mwdev/open/ranker/MWRanker.java
+++ src/main/java/com/mwdev/open/ranker/MWRanker.java
@@ -1,12 +1,36 @@
 package com.mwdev.open.ranker;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 /**
  * @author Plez Ownby
  */
 public class MWRanker {
 
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("stats.txt"));
+            writer.write("writing to a file.");
+            writer.write("\nthis is a new line");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("stats.txt"));
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+                
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
